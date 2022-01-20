d6c1e3d41e0bb01888cc863b67e713ebb8ff2d5c
Cdric_Art <cdric.art@gmail.com>
2022-01-20 20:50:11 +0100

    Feat: SideBar component

---
+++ components/SideBar.tsx
@@ -0,0 +1,57 @@
+import Image from "next/image";
+import React from "react";
+import { styled } from "../styles/stitches.config";
+
+import { ListLink } from "./sidebar/ListLink";
+import { ListSocial } from "./sidebar/ListSocial";
+
+import LogoImage from "../public/assets/icons/LogoC.png";
+
+export const SideBar = () => {
+  return (
+    <Side_Bar>
+      <Logo_container href="/">
+        <Image src={LogoImage} alt="logo" height={120} width={50} />
+        <h3>Cédric</h3>
+        <h4>Développeur Web</h4>
+      </Logo_container>
+      <ListLink />
+      <ListSocial />
+    </Side_Bar>
+  );
+};
+
+const Side_Bar = styled("nav", {
+  display: "none",
+  "@desktop": {
+    display: "initial",
+    position: "fixed",
+    top: 0,
+    left: 0,
+    height: "100vh",
+    backgroundColor: "$dark100",
+  },
+});
+
+const Logo_container = styled("a", {
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  backgroundColor: "black",
+  "& img": {
+    marginTop: "$space3!important",
+    objectFit: "contain",
+  },
+  "& h3": {
+    fontSize: "1.5rem",
+    fontWeight: "700",
+    color: "white",
+  },
+  "& h4": {
+    fontSize: ".8rem",
+    fontWeight: "600",
+    color: "$gray50",
+    padding: "$space3",
+  },
+});
---
+++ components/sidebar/ListLink.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import Link from "next/link";
+import { styled } from "../../styles/stitches.config";
+
+export const ListLink = () => {
+  return (
+    <List>
+      <li>
+        <Link href="/#about">
+          <a>A propos</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/#skills">
+          <a>Mes compétences</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/#projects">
+          <a>Mes projets</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/#contact">
+          <a>Contact</a>
+        </Link>
+      </li>
+    </List>
+  );
+};
+
+const List = styled("ul", {
+  display: "flex",
+  alignItems: "center",
+  flexDirection: "column",
+  justifyContent: "center",
+  marginTop: "80px",
+
+  "& li": {
+    width: "100%",
+    textAlign: "center",
+    borderTop: "1px solid rgba(255, 255, 255, 0.05)",
+
+    "& a": {
+      display: "block",
+      fontSize: "1rem",
+      transition: "all .3s",
+      paddingInline: "$space3",
+      paddingBlock: "$space4",
+
+      "&:hover": {
+        color: "$contrast",
+      },
+    },
+  },
+});
